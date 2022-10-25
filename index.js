139323ad0275c4c080cacc41d036398760d7d6a3
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-26 00:26:05 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
-}
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+  }
+  function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+  }
+  const leastCustomer = "bob"
+  function changeLeastFavoriteCustomer() {
+    return leastCustomer = 'someelse'
+  }
